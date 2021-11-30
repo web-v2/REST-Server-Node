4406a808cbcf14b4c49c83e2db0b6927603a3ced
Samir Vergara V <svergara3@misena.edu.co>
2021-11-29 23:32:02 -0500

    Google Sing In - Creado exitosamente

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const { response } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT} = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+
 const login = async (req, res = response) => {
     const {correo, password} = req.body;
     try {
@@ -35,5 +37,43 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSingIn = async (req, res= response) => {
+    const {id_token} = req.body;
+    try {
 
-module.exports = {login}
+        const {nombre, img, correo} = await googleVerify(id_token);
+        
+        let usuario = await Usuario.findOne({correo});
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo, 
+                password: 'regiter google',
+                rol: 'USER_ROLE',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if (!usuario.estado){
+            return res.status(401).json({
+                message: 'Error, usuario bloqueado!'
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT(usuario.id);
+        res.json({usuario,token});     
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            message: 'Error, no se pudo verificar el Token'
+        });
+    }
+}
+
+module.exports = {login,googleSingIn}
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login,googleSingIn } = require('../controllers/auth');
 const { validarCampos } = require('../middleware/validar-campos');
 
 const router = Router();
@@ -10,5 +10,10 @@ router.post('/login', [
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ],login);
+
+router.post('/google', [
+    check('id_token', 'Token de google es necesario').not().isEmpty(),    
+    validarCampos
+],googleSingIn);
 
 module.exports = router;
